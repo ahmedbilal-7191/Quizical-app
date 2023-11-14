6646f4866255c5d60739678fea73799252f1309b
bilal ahmed <ahmed.bilal7191@gmail.com>
2023-11-14 16:52:34 +0530

    initial

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+const data={
+    "response_code": 0,
+        "results": [{
+            "category": "Celebrities",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Named after a character he played in a 1969 film, what is the name of the ski resort in Utah that Robert Redford bought in 1968?",
+            "correct_answer": "Sundance",
+            "incorrect_answers": ["Woodward", "Turner", "Booker"]
+        },
+        {
+            "category": "Celebrities",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What was the cause of Marilyn Monroes suicide?",
+            "correct_answer": "Drug Overdose",
+            "incorrect_answers": ["Knife Attack", "House Fire", "Gunshot"]
+        },
+        {
+            "category": "Celebrities",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which celebrity announced his presidency in 2015?",
+            "correct_answer": "Kanye West",
+            "incorrect_answers": ["Donald Trump", "Leonardo DiCaprio", "Miley Cyrus"]
+        },
+        {
+            "category": "Celebrities",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Aubrey Graham is better known as",
+            "correct_answer": "Drake",
+            "incorrect_answers": ["Travis Scott", "Lil Wayne", "2 Chainz"]
+        },
+        {
+            "category": "Celebrities",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Neil Hamburger is played by which comedian?",
+            "correct_answer": "Gregg Turkington",
+            "incorrect_answers": ["Nathan Fielder", "Tim Heidecker", "Todd Glass"]
+        }]
+}
+export default data
